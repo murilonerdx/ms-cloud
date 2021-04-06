b1e1da53fec48254fa18c0b7030e69dff5e9dbbb
Murilonerdx <mu-silva@outlook.com>
2021-04-06 19:49:42 -0300

    Adicionando projeto hr-worker

---
+++ hr-worker/src/main/java/com/murilo/hrworker/HrworkerApplication.java
@@ -0,0 +1,13 @@
+package com.murilo.hrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrworkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrworkerApplication.class, args);
+    }
+
+}
---
+++ hr-worker/src/test/java/com/murilo/hrworker/HrworkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murilo.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrworkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
