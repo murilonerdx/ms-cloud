8028cc24629e6ab66396959622a0b27a7d358726
Murilonerdx <mu-silva@outlook.com>
2021-04-07 16:09:17 -0300

    Feign client, implementando hr-worker com Cloud e open feign

--- hr-payroll/src/main/java/com/murilo/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/murilo/hrpayroll/services/PaymentService.java
@@ -2,6 +2,7 @@ package com.murilo.hrpayroll.services;
 
 import com.murilo.hrpayroll.entities.Payment;
 import com.murilo.hrpayroll.entities.Worker;
+import com.murilo.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -12,19 +13,23 @@ import java.util.Map;
 
 @Service
 public class PaymentService {
+//
+//    @Value("${hr-worker.host}")
+//    private String wokerHost;
 
-    @Value("${hr-worker.host}")
-    private String wokerHost;
+//    @Autowired
+//    private RestTemplate restTemplate;
+
 
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
 
     public Payment getPayment(long workId, int days){
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", ""+workId);
-
-        Worker worker = restTemplate.getForObject(wokerHost+"/workers/{id}", Worker.class, uriVariables);
+//        Map<String, String> uriVariables = new HashMap<>();
+//        uriVariables.put("id", ""+workId);
+        Worker worker = workerFeignClient.findById(workId).getBody();
+//        Worker worker = restTemplate.getForObject(wokerHost+"/workers/{id}", Worker.class, uriVariables);
 
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
