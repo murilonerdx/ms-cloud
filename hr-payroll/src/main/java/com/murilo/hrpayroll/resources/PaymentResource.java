69d25d91210b348467c8c18663eac72a88345de2
Murilonerdx <mu-silva@outlook.com>
2021-04-08 16:30:36 -0300

    Hystrix para tolerância a falhas e erros

--- hr-payroll/src/main/java/com/murilo/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/murilo/hrpayroll/resources/PaymentResource.java
@@ -2,6 +2,7 @@ package com.murilo.hrpayroll.resources;
 
 import com.murilo.hrpayroll.entities.Payment;
 import com.murilo.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,15 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/payments")
 public class PaymentResource {
 
-	@Autowired
-	private PaymentService service;
-	
-	@GetMapping(value = "/{workerId}/days/{days}")
-	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
-		Payment payment = service.getPayment(workerId, days);
-		return ResponseEntity.ok(payment);
-	}	
-}
+    @Autowired
+    private PaymentService service;
+
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
+    @GetMapping(value = "/{workerId}/days/{days}")
+    public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
+        Payment payment = service.getPayment(workerId, days);
+        return ResponseEntity.ok(payment);
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+        Payment payment = new Payment("Brann",400.0, days);
+        return ResponseEntity.ok(payment);
+    }
+    }
 
 
 
