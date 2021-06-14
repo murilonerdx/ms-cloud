45d1c775035ed7a837f7749eb945442b1876a545
Murilonerdx <mu-silva@outlook.com>
2021-06-14 19:08:57 -0300

    Configuration JWT eureka and service hr-oauth, Login e token JWT PARTE 2/2

--- hr-oauth/src/main/java/com/murilo/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/murilo/hroauth/services/UserService.java
@@ -5,10 +5,13 @@ import com.murilo.hroauth.feignclientes.UserFeignClientes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -22,6 +25,17 @@ public class UserService {
             throw new IllegalAccessException("Email not found");
         }
         logger.info("Email found: " + email);
+        return user;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        User user = userFeignClientes.findByEmail(s).getBody();
+        if(user == null){
+            logger.error("Username not found: " + s);
+            throw new UsernameNotFoundException("Username not found");
+        }
+        logger.info("Username found: " + s);
         return user;
     }
 }
